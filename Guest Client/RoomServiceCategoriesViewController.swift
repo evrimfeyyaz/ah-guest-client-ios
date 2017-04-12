6956f76417105f76e6763641fe2ca58328de99ac
Evrim P <evrimfeyyaz@gmail.com>
2017-04-12 11:50:47 +0300

    Create a room service categories view controller and a storyboard for room service.

---
+++ Guest Client/RoomServiceCategoriesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RoomServiceCategoriesViewController.swift
+//  Guest Client
+//
+//  Created by Evrim Persembe on 4/12/17.
+//  Copyright © 2017 Automated Hotel. All rights reserved.
+//
+
+import UIKit
+
+class RoomServiceCategoriesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
